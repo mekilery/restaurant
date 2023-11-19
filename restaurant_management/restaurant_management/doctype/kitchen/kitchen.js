dc74d3b03976e609d5ca3abdd411f2d08a92cd84
Alireza <eng.alirezafaraji@gmail.com>
2023-11-19 07:14:40 +0000

    update 01

--- restaurant_management/restaurant_management/doctype/kitchen/kitchen.js
+++ restaurant_management/restaurant_management/doctype/kitchen/kitchen.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2021, Quantum Bit Core and contributors
+// Copyright (c) 2023, Quantum Bit Core and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Table Order', {
+frappe.ui.form.on('Kitchen', {
 	// refresh: function(frm) {
 
 	// }
---
+++ restaurant_management/restaurant_management/doctype/kitchen/kitchen.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Quantum Bit Core and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Kitchen(Document):
+	pass
